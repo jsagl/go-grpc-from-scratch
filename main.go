d34ceccb73f2a99d8a7a458aa816a0ad245b352d
Jean Saglio <jean.saglio@gmail.com>
2020-04-04 18:18:51 +0200

    should work on heroku but no longer locally as environement variables cannot be read

--- main.go
+++ main.go
@@ -3,30 +3,29 @@ package main
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"github.com/jsagl/go-grpc-from-scratch/api/service/v1"
 	"github.com/jsagl/go-grpc-from-scratch/server/grpc"
 	"github.com/jsagl/go-grpc-from-scratch/server/rest"
 	"github.com/jsagl/go-grpc-from-scratch/storage/postgres"
 	_ "github.com/lib/pq"
-	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
-	viper.SetConfigFile(".env")
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			fmt.Println("No .env file was found")
-		} else {
-			log.Fatalf("Error while reading config file %s", err)
-		}
-	}
+	//viper.SetConfigFile(".env")
+	//if err := viper.ReadInConfig(); err != nil {
+	//	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+	//		fmt.Println("No .env file was found")
+	//	} else {
+	//		log.Fatalf("Error while reading config file %s", err)
+	//	}
+	//}
 
 
 	ctx := context.Background()
 
-	dbAddress := viper.Get("DATABASE_URL").(string)
+	dbAddress := os.Getenv("DATABASE_URL")
 	db, err := sql.Open("postgres", dbAddress)
 	if err != nil {
 		log.Panic(err)
@@ -41,8 +40,8 @@ func main() {
 	recipeStore := storage.NewPostgresRecipeStore(db)
 	v1RecipeServiceServer := v1.NewRecipeServiceServer(recipeStore)
 
-	httpPort := viper.Get("PORT").(string)
-	grpcPort := viper.Get("GRPC_PORT").(string)
+	httpPort := os.Getenv("PORT")
+	grpcPort := os.Getenv("GRPC_PORT")
 
 	// run HTTP gateway
 	go func() {
